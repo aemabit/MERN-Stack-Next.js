e645ab29f34b29e00d0b642d4f4d6de958395d90
Andres Mejias <aemabit@gmail.com>
2019-12-22 14:55:25 -0500

    pagination products

--- components/Index/ProductPagination.js
+++ components/Index/ProductPagination.js
@@ -1,5 +1,27 @@
-function ProductPagination() {
-  return <>ProductPagination</>;
+import { useRouter } from 'next/router'
+import { Container, Pagination } from 'semantic-ui-react'
+
+
+function ProductPagination({ totalPages }) {
+
+  const router = useRouter()
+
+  return(
+    <Container textAlign="center" style={{ margin: '2em' }}>
+      <Pagination
+        defaultActivePage={1}
+        ellipsisItem={null}
+        firstItem={null}
+        lastItem={null}
+        totalPages={totalPages}
+        onPageChange={(event, data) => {
+          data.activePage === 1
+            ? router.push('/')
+            : router.push(`/?page=${data.activePage}`);
+        }}
+      />
+    </Container>
+  )
 }
 
 export default ProductPagination;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import axios from 'axios'
 import ProductList from '../components/Index/ProductList'
+import ProductPagination from '../components/Index/ProductPagination'
 import baseUrl from '../utils/baseUrl'
 
-function Home({ products }) {
-  return <ProductList products={products} />
+function Home({ products, totalPages }) {
+  return (
+    <>
+      <ProductList products={products} />
+      <ProductPagination totalPages={totalPages} />
+    </>
+    )
 }
 
-Home.getInitialProps = async () => {
-  // fetch data on server
+Home.getInitialProps = async ctx => {
+  const page = ctx.query.page ? ctx.query.page : "1"
+  const size = 12;
   const url =`${baseUrl}/api/products`
-  const res = await axios.get(url)
-  return {products: res.data}
+  const payload = { params: { page, size } }
+  // fetch data on server
+  const res = await axios.get(url, payload)
   // returns response data as an object
+  return res.data
 }
 
 export default Home;
