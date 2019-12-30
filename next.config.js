42ae0b69a24fcd01fd0d007f59f58f5abeb58eb1
Andres Mejias <aemabit@gmail.com>
2019-12-29 21:37:38 -0500

    fixed untracked files

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// must restart server whenever you make changes in next.config
-module.exports = {
-  env: {
-    MONGO_SRV: "mongodb+srv://gtMasterDB:jeywnMUg8UQ2HVq4@goldtokendb-bw48i.mongodb.net/test?retryWrites=true&w=majority",
-    JWT_SECRET: "goldtreetoken",
-    CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/aemabit/image/upload",
-    STRIPE_SECRET_KEY: "sk_test_wP4qBOcVqfpI8SR22jGrZ4rv00cDs5aOso"
-  }
-};
